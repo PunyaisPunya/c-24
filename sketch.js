bcad0057d50362d3bd0edf1b16cf7f027f796b37
PunyaIsPunya <87646487+PunyaisPunya@users.noreply.github.com>
2021-09-08 15:54:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+
+var tower;
+var angle;
+var cannon;
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
+
+}
+function setup() {
+  createCanvas(1200,600);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  tower = new Tower(150, 350, 160, 310);
+  angle = -PI/4;
+  
+  cannon = new Canon(180,110,100,50,angle);
+
+
+
+
+}
+
+function draw() 
+{
+  background(51);
+  image(backgroundImg,0,0,width,height);
+  Engine.update(engine);
+    tower.display();
+    cannon.display();
+
+ 
+}
+
